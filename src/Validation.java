03000fc90b4b2ab36f3a58625064f1098b05b95c
YumgyulAhmedova <yumi_1991_@abv.bg>
2022-11-22 20:04:57 +0200

    Validations and search employee by name method

---
+++ src/Validation.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Validation {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static String checkIsValidInput() {
+        String input = null;
+        boolean isValidInput = true;
+        while (isValidInput) {
+            input = scanner.nextLine().trim();
+            if (checkInputIsNumber(input)){
+                continue;
+            }
+            if (input.trim().isEmpty()) {
+                System.out.println("Input cannot be empty! Try again!");
+                continue;
+            }
+            isValidInput = false;
+        }
+        return input;
+    }
+
+    static boolean checkInputIsNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                System.out.println("Input cannot be an integer! Try again!");
+                return true;
+            }
+        }
+        return false;
+    }
+}
